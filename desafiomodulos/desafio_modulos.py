60dc837a5742a6a59ad7276940103512150c2ca0
frankito <lazofrancom@gmail.com>
2025-10-18 10:40:18 -0300

    Delete desafiomodulos directory

--- desafiomodulos/desafio_modulos.py
+++
@@ -1,8 +0,0 @@
-from Cursada.modulos.input import *
-
-
-get_int(f"Hola, vamos a solicitar un entero para validar.", "El entero que ingresaste esta fuera de los límites.", 1, 5, 3)
-
-# get_float("Este es un numero flotante", "Este es el error", 1, 50, 2)
-
-# get_string(8)
